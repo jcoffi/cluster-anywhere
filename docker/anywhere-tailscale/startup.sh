266b54c4649aacc2553ef20442aed0eef5fcb80a
Justin Coffi <jcoffi+github@gmail.com>
2023-02-25 16:27:52 -0800

    re-disabled ipv6

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-#echo "net.ipv6.conf.all.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
-#echo "net.ipv6.conf.default.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
-#echo "net.ipv6.conf.lo.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6=1" | sudo tee -a /etc/sysctl.conf
 echo "vm.max_map_count = 262144" | sudo tee -a /etc/sysctl.conf
 echo "vm.swappiness = 1" | sudo tee -a /etc/sysctl.conf
 
