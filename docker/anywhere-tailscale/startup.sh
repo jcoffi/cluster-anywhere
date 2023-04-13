e3451255f3716f2f464f7d26dcb25e189d16ffbf
Justin Coffi <jcoffi+github@gmail.com>
2023-04-12 23:07:05 -0700

    sudo chmod +R 777 ~/files

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -216,7 +216,8 @@ fi
 
 
 if $(grep -q microsoft /proc/version); then
-  conda install -c conda-forge -y jupyterlab nano #&& jupyter-lab --allow-root --notebook-dir /files --ip 0.0.0.0 --no-browser --preferred-dir /files &
+  sudo chmod +R 777 ~/files
+  conda install -c conda-forge -y jupyterlab nano && jupyter-lab --allow-root --notebook-dir /files --ip 0.0.0.0 --no-browser --preferred-dir /files &
 fi
 
 
