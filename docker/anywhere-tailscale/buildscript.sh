51049f02594cfe0e130af337b17eb62484145ed5
Justin Coffi <jcoffi+github@gmail.com>
2023-04-28 09:10:53 -0700

    refactored buildscript.sh

--- docker/anywhere-tailscale/buildscript.sh
+++ docker/anywhere-tailscale/buildscript.sh
@@ -1,126 +1,164 @@
 #!/bin/bash
-if ! [ -n $DOCKER_PASSWORD ]; then
-  echo "Environmental variable for DOCKER_PASSWORD not set"
+
+# Check if the DOCKER_PASSWORD environment variable is set
+if [ -z "$DOCKER_PASSWORD" ]; then
+  echo "Environment variable for DOCKER_PASSWORD not set"
   exit 1
 fi
 
+# Variables
 builddir="/home/tripps/build"
-#PLATFORM="$( case $(uname --m) in x86_64) echo x64_linux ;; aarch64) echo aarch64_linux ;; esac)"
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 distro=${distribution//\./}
 
-if [ -d $builddir/ ]; then
-    cd $builddir/
-    sudo rm -rf *
-else
-    sudo mkdir -p $builddir /home/tripps/data && cd $builddir
-fi
+# Functions
+disable_nouveau() {
+  # Create a new configuration file for the kernel modules
+  echo "Creating configuration file to disable Nouveau drivers..."
+  echo "blacklist nouveau" | sudo tee /etc/modprobe.d/disable-nouveau.conf > /dev/null
+  echo "options nouveau modeset=0" | sudo tee -a /etc/modprobe.d/disable-nouveau.conf > /dev/null
 
-if [ -x /usr/bin/podman ]; then
-    DOCKER_HOST=`echo "unix://${XDG_RUNTIME_DIR}/podman/podman.sock"`
+  # Regenerate the initial RAM disk (initramfs) to apply the changes
+  echo "Regenerating initramfs..."
+  sudo update-initramfs -u
+
+  # Prompt user to reboot
+  echo "Please reboot your system to apply the changes:"
+  echo "sudo reboot"
+}
+
+prepare_build_directory() {
+  if [ -d "$builddir/" ]; then
+    cd "$builddir/"
+    sudo rm -rf *
+  else
+    sudo mkdir -p "$builddir" /home/tripps/data && cd "$builddir"
+  fi
+}
+
+setup_docker_or_podman() {
+  if [ -x /usr/bin/podman ]; then
+    DOCKER_HOST="unix://${XDG_RUNTIME_DIR}/podman/podman.sock"
     export DOCKER_HOST
     exec=/usr/bin/podman
     sudo podman registry remove quay.io
-    podman login --username jcoffi --password $DOCKER_PASSWORD
+    podman login --username jcoffi --password "$DOCKER_PASSWORD"
     alias docker=podman
-fi
-
-if [ -x /usr/bin/docker ]; then
+  elif [ -x /usr/bin/docker ]; then
     exec=/usr/bin/docker
-    docker login --username jcoffi --password $DOCKER_PASSWORD
-fi
+    docker login --username jcoffi --password "$DOCKER_PASSWORD"
+  fi
+}
 
-sudo sysctl -w vm.max_map_count=262144
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
-sudo sysctl -p
+configure_sysctl() {
+  sudo sysctl -w vm.max_map_count=262144
+  sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+  sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+  sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+  sudo sysctl -p
 
-if [ -d /sys/class/power_supply/BAT0 ]; then
+  echo -e "vm.max_map_count = 262144\n" | sudo tee /etc/sysctl.conf
+  echo -e "net.ipv6.conf.all.disable_ipv6=1\n" | sudo tee /etc/sysctl.conf
+  echo -e "net.ipv6.conf.default.disable_ipv6=1\n" | sudo tee /etc/sysctl.conf
+  echo -e "net.ipv6.conf.lo.disable_ipv6=1\n" | sudo tee /etc/sysctl.conf
+}
+
+disable_sleep_for_laptops() {
+  if [ -d /sys/class/power_supply/BAT0 ]; then
     echo "Script is running on a laptop"
     #disable sleep on laptops
     sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
-fi
+  fi
+}
 
-echo -e "vm.max_map_count = 262144\n" | sudo tee /etc/sysctl.conf
-echo -e "net.ipv6.conf.all.disable_ipv6=1\n" | sudo tee /etc/sysctl.conf
-echo -e "net.ipv6.conf.default.disable_ipv6=1\n" | sudo tee /etc/sysctl.conf
-echo -e "net.ipv6.conf.lo.disable_ipv6=1\n" | sudo tee /etc/sysctl.conf
+install_docker_on_wsl() {
+  if ! [ -x "$(command -v docker)" ] && ! [ -z "$WSL_DISTRO_NAME" ] && ! [ -x /usr/bin/podman ]; then
+    sudo curl https://get.docker.com | sh
+    sudo adduser ubuntu docker
+  fi
+}
 
-# Get the total amount of memory in kB
-memory=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+remove_registry_and_install_dependencies() {
+  if [ -x /usr/bin/podman ]; then
 
-# Convert kB to GB
-gb_memory=$(echo "scale=2; $memory / 1048576" | bc)
-
-shm_memory=$(echo "scale=2; $gb_memory / 3" | bc)
-
-if ! [ -x "$(command -v docker)" ] && ! [ -z "$WSL_DISTRO_NAME" ] && ! [ -x /usr/bin/podman ]; then
-sudo curl https://get.docker.com | sh
-sudo adduser ubuntu docker
-fi
-
-if [ -x /usr/bin/podman ]; then
-  sudo podman registry remove quay.io
-fi
-
-sudo apt install --no-install-recommends -y jq wget && sudo apt -y autoremove
-
-# Check if the GPU is NVIDIA
-if [ -n "$(lspci | grep -i nvidia)" ] || [ -n "$(nvidia-smi -L)" ]; then
-
-
-  if [ -x "$(command -v nvidia-smi)" ] && [ -d /usr/local/cuda ]; then
-    CUDA="Already done"
-    # Get the driver version
-    nvidia_driver_ver=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
-  elif [ -n "$WSL_DISTRO_NAME" ]; then
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
-    wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/$(uname --m)/cuda-keyring_1.0-1_all.deb -O cuda-keyring_1.0-1_all.deb && sudo chmod +x cuda-keyring_1.0-1_all.deb
-    sudo apt install cuda-keyring_1.0-1_all.deb
-    sudo apt update
-    sudo apt -y install cuda
-    CUDA="WSL"
-  elif [ -n "$(lspci | grep -i nvidia)" ]; then
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
-    wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$(uname --m)/cuda-keyring_1.0-1_all.deb -O cuda-keyring_1.0-1_all.deb && sudo chmod +x cuda-keyring_1.0-1_all.deb
-    sudo dpkg -i cuda-keyring_1.0-1_all.deb
-    sudo apt-get update
-    sudo apt-get -y install cuda
-    CUDA="lspci"
+    sudo podman registry remove quay.io
   fi
 
-  if [[ -n $CUDA ]] && [ -f /usr/local/cuda/version.json ]; then
-    # Get the CUDA version
-    cuda_version=$(cat /usr/local/cuda/version.json | jq -r '.cuda.version')
+  sudo apt install --no-install-recommends -y jq wget && sudo apt -y autoremove
+}
 
-    # Strip out the decimal point
-    cuda_version=${cuda_version//\./}
-    #i dont have time to figure this out right now
-    echo $cuda_version
-    if [ $cuda_version == "1200" ]; then
-      cuda_version="116"
+handle_nvidia_gpu() {
+  # Check if the GPU is NVIDIA
+  if [ -n "$(lspci | grep -i nvidia)" ] || [ -n "$(nvidia-smi -L)" ]; then
+    if [ -x "$(command -v nvidia-smi)" ] && [ -d /usr/local/cuda ]; then
+      CUDA="Already done"
+      # Get the driver version
+      nvidia_driver_ver=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader)
+    elif [ -n "$WSL_DISTRO_NAME" ]; then
+      install_nvidia_cuda "wsl-ubuntu"
+      CUDA="WSL"
+    elif [ -n "$(lspci | grep -i nvidia)" ]; then
+      install_nvidia_cuda "$distro"
+      CUDA="lspci"
     fi
-  elif [[ -n $CUDA ]] && ! [ -f /usr/local/cuda/version.json ]; then
-    #we will default to 11.2
-    cuda_version="gpu"
+
+    if [[ -n $CUDA ]] && [ -f /usr/local/cuda/version.json ]; then
+      # Get the CUDA version
+      cuda_version=$(cat /usr/local/cuda/version.json | jq -r '.cuda.version')
+
+      # Strip out the decimal point
+      cuda_version=${cuda_version//\./}
+      #i dont have time to figure this out right now
+      echo $cuda_version
+      if [ $cuda_version == "1200" ]; then
+        cuda_version="116"
+      fi
+    elif [[ -n $CUDA ]] && ! [ -f /usr/local/cuda/version.json ]; then
+      #we will default to 11.2
+      cuda_version="gpu"
+    fi
+  fi
+}
+
+install_nvidia_cuda() {
+  local repo="$1"
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
+  wget https://developer.download.nvidia.com/compute/cuda/repos/$repo/$(uname --m)/cuda-keyring_1.0-1_all.deb -O cuda-keyring_1.0-1_all.deb && sudo chmod +x cuda-keyring_1.0-1_all.deb
+  sudo dpkg -i cuda-keyring_1.0-1_all.deb
+  sudo apt-get update
+  sudo apt-get -y install cuda
+}
+
+download_docker_files() {
+  wget https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/Dockerfile -O Dockerfile
+  wget https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/startup.sh -O "$builddir/startup.sh"
+  sudo chmod 777 Dockerfile
+  sudo chmod 777 "$builddir/startup.sh"
+}
+
+build_and_push_docker_image() {
+  if [[ -n $cuda_version ]] && [ $cuda_version != "gpu" ]; then
+    sudo $exec build --cache-from=index.docker.io/rayproject/ray-ml:2.2.0-py38-cu$cuda_version $builddir -t docker.io/jcoffi/cluster-anywhere:cu$cuda_version --build-arg IMAGETYPE=cu$cuda_version
+    sudo $exec push  docker.io/jcoffi/cluster-anywhere:cu$cuda_version
+  elif [[ -n $cuda_version ]] && [ $cuda_version == "gpu" ]; then
+    sudo $exec build --cache-from=index.docker.io/rayproject/ray-ml:2.2.0
+    sudo $exec build --cache-from=index.docker.io/rayproject/ray-ml:2.2.0-py38-gpu $builddir -t docker.io/jcoffi/cluster-anywhere:gpu -t docker.io/jcoffi/cluster-anywhere:gpu-latest --build-arg IMAGETYPE=gpu
+    sudo $exec push docker.io/jcoffi/cluster-anywhere:gpu
+  else
+    sudo $exec build --cache-from=index.docker.io/rayproject/ray:2.2.0-py38-cpu $builddir -t docker.io/jcoffi/cluster-anywhere:cpu -t docker.io/jcoffi/cluster-anywhere:latest -t docker.io/jcoffi/cluster-anywhere:cpu-latest --build-arg IMAGETYPE=cpu
+    sudo $exec push docker.io/jcoffi/cluster-anywhere:$cuda_version
   fi
 
-fi
-
-wget https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/Dockerfile -O Dockerfile && wget https://raw.githubusercontent.com/jcoffi/cluster-anywhere/master/docker/anywhere-tailscale/startup.sh -O $builddir/startup.sh && sudo chmod 777 $builddir/Dockerfile && sudo chmod 777 $builddir/startup.sh
-
-if [[ -n $cuda_version ]] && [ $cuda_version != "gpu" ]; then
-  sudo $exec build --cache-from=index.docker.io/rayproject/ray-ml:2.2.0-py38-cu$cuda_version $builddir -t docker.io/jcoffi/cluster-anywhere:cu$cuda_version --build-arg IMAGETYPE=cu$cuda_version
-  sudo $exec push  docker.io/jcoffi/cluster-anywhere:cu$cuda_version
-elif [[ -n $cuda_version ]] && [ $cuda_version == "gpu" ]; then
-  sudo $exec build --cache-from=index.docker.io/rayproject/ray-ml:2.2.0-py38-gpu $builddir -t docker.io/jcoffi/cluster-anywhere:gpu -t docker.io/jcoffi/cluster-anywhere:gpu-latest --build-arg IMAGETYPE=gpu
-  sudo $exec push docker.io/jcoffi/cluster-anywhere:gpu
-else
-  sudo $exec build --cache-from=index.docker.io/rayproject/ray:2.2.0-py38-cpu $builddir -t docker.io/jcoffi/cluster-anywhere:cpu -t docker.io/jcoffi/cluster-anywhere:latest -t docker.io/jcoffi/cluster-anywhere:cpu-latest --build-arg IMAGETYPE=cpu
-  sudo $exec push docker.io/jcoffi/cluster-anywhere:$cuda_version
-fi
-
-if [ -f /root/.docker/config.json ]; then
+  if [ -f /root/.docker/config.json ]; then
     sudo rm /root/.docker/config.json
-fi
+  fi
+}
+
+check_environment_variable
+prepare_builddir
+check_installed_executables
+configure_system_settings
+install_dependencies
+handle_nvidia_gpu
+download_docker_files
+build_and_push_docker_image
