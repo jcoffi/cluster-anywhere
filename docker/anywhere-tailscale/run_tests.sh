fce200f02df59f5694cff91407ec32336b5b9833
Justin Coffi <jcoffi+github@gmail.com>
2023-04-30 12:48:39 -0700

    fixed the logic

--- docker/anywhere-tailscale/run_tests.sh
+++ docker/anywhere-tailscale/run_tests.sh
@@ -6,8 +6,11 @@ crash -U crate -c "SELECT * FROM sys.nodes" || exit 1
 
 
 #ray status || exit 1
-if [ ! "$NODETYPE" = "head" ]; then
+if [ "$NODETYPE" = "head" ]; then
         ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE | grep -q ALIVE || ray start --head --num-cpus=0 --num-gpus=0 --disable-usage-stats --include-dashboard=True --dashboard-host 0.0.0.0 --node-ip-address nexus.chimp-beta.ts.net || exit 1
 else
         ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE | grep -q ALIVE || ray start --address='nexus.chimp-beta.ts.net:6379' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net || exit 1
 fi
+
+
+exit 0
