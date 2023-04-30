615c9c68e1a9242b6ad8b57cfc0aec03040fad2d
Justin Coffi <jcoffi+github@gmail.com>
2023-04-30 11:17:05 -0700

    added multipart download and removed the exist 0 from test

--- docker/anywhere-tailscale/run_tests.sh
+++ docker/anywhere-tailscale/run_tests.sh
@@ -7,6 +7,3 @@ crash -U crate -c "SELECT * FROM sys.nodes" || exit 1
 
 #ray status || exit 1
 ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE | grep -q ALIVE || exit 1
-
-
-exit 0
