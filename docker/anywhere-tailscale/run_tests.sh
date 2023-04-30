5877162ece3d1d3b4d7b18dd672d8b10fd93b000
Justin Coffi <jcoffi+github@gmail.com>
2023-04-30 11:25:16 -0700

    changed retries and made run tests restart ray if it fails

--- docker/anywhere-tailscale/run_tests.sh
+++ docker/anywhere-tailscale/run_tests.sh
@@ -6,4 +6,8 @@ crash -U crate -c "SELECT * FROM sys.nodes" || exit 1
 
 
 #ray status || exit 1
-ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE | grep -q ALIVE || exit 1
+if [ ! "$NODETYPE" = "head" ]; then
+        ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE | grep -q ALIVE || ray start --head --num-cpus=0 --num-gpus=0 --disable-usage-stats --include-dashboard=True --dashboard-host 0.0.0.0 --node-ip-address nexus.chimp-beta.ts.net
+else
+        ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE | grep -q ALIVE || ray start --address='nexus.chimp-beta.ts.net:6379' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net
+fi
