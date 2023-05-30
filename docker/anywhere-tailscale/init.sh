675dcbbf2646bcb837443f3be93d1a3d30a887d0
Justin Coffi <jcoffi+github@gmail.com>
2023-05-30 16:31:41 -0700

    moving startup launch to init.sh

---
+++ docker/anywhere-tailscale/init.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+/usr/bin/wget -o /home/ray/startup.sh "https://raw.githubusercontent.com/jcoffi/ray/cluster-anywhere/docker/anywhere-tailscale/startup.sh" && /bin/sh -c /home/ray/startup.sh
