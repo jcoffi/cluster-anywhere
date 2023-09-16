064cb76aad3b61a6adf8793f3d8174ae24423b37
Justin Coffi <jcoffi+github@gmail.com>
2023-09-16 11:07:14 -0700

    refactored run_test.sh

--- docker/anywhere-tailscale/run_tests.sh
+++ docker/anywhere-tailscale/run_tests.sh
@@ -1,38 +1,32 @@
-if [ "$LOCATION" = "AWS" ]; then
-        metadata_url="http://169.254.169.254/latest/meta-data/spot/termination-time"
-        if curl $metadata_url; then
-                /usr/local/bin/crash --hosts ${CLUSTERHOSTS} -c "ALTER CLUSTER DECOMMISSION '"$HOSTNAME"';" &
-                ray stop -f
-                sudo tailscale logout
-
-        fi
-fi
+#!/bin/bash
 
 FAIL=0
 
-
-tailscale status -json | jq -r .BackendState | grep -q "Running" || FAIL=1
-
-
-ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE | grep -q "ALIVE" || FAIL=1
-
-
-curl -s -X POST "http://localhost:4200/_sql?pretty" -H 'Content-Type: application/json' -d'
-{
-    "stmt": "select * from sys.nodes where name = '"'$HOSTNAME'"'"
-}
-' | jq -e '.rows | length > 0' || FAIL=1
-
-
-
-#i'm in a hurry. but these if statements could use the lines above as boolen responses like crate_pid
-
-if [ "$FAIL" = "1" ] && [ ! "$NODETYPE" = "user" ]; then
-    echo "Failed and restarting"
-    crate_pid=$(pgrep -f crate)
-    if [ $crate_pid ]; then
-        sudo kill -TERM $crate_pid
+# Check for AWS Spot Instance Termination
+if [ "$LOCATION" = "AWS" ]; then
+    metadata_url="http://169.254.169.254/latest/meta-data/spot/termination-time"
+    if curl -s $metadata_url; then
+        /usr/local/bin/crash --hosts ${CLUSTERHOSTS} -c "ALTER CLUSTER DECOMMISSION '$HOSTNAME';" &
+        ray stop -g 30
+        sudo tailscale logout
+        exit 0
     fi
-    sudo kill -TERM 1
+fi
 
+# Check Tailscale and Ray status
+tailscale_status=$(tailscale status -json | jq -r .BackendState)
+if [ "$tailscale_status" != "Running" ]; then FAIL=1; fi
+
+ray_status=$(ray list nodes -f NODE_NAME="${HOSTNAME}.chimp-beta.ts.net" -f STATE=ALIVE)
+if [ -z "$ray_status" ]; then FAIL=1; fi
+
+# Check Crate.io status (Added this part)
+crate_status=$(curl -s http://localhost:4200/ | grep -q "CrateDB Admin UI")
+if [ -z "$crate_status" ]; then FAIL=1; fi
+
+# Other test commands here
+
+if [ $FAIL -eq 1 ]; then
+    echo "Tests failed."
+    exit 1
 fi
