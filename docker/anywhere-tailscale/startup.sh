644713e938ca710e72c1cb5c0f9fd58ed682301d
Justin Coffi <jcoffi+github@gmail.com>
2023-09-18 14:10:29 -0700

    added string deduplication java vm options and updated the startup.sh so that off prem containers make 4200 available

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -332,13 +332,13 @@ elif [ "$NODETYPE" = "user" ]; then
   #but we can't move it to the head node right now because the only other port is 10001 and that conflicts with ray
   sudo tailscale serve https:8443 / http://localhost:8888 \
   && sudo tailscale funnel 8443 on
-  #eventually we can make the below lines available to all nodetypes for cluster health checks. but first we need to configured the instances to connect was a password when connecting remotely.
-  sudo tailscale serve https:443 / http://localhost:4200 \
-  && sudo tailscale funnel 443 on
 else
 
   if [ "$LOCATION" != "OnPrem" ]; then
     ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
+    #eventually we can make the below lines available to all nodetypes for cluster health checks. but first we need to configured the instances to connect was a password when connecting remotely.
+    sudo tailscale serve https:443 / http://localhost:4200 \
+    && sudo tailscale funnel 443 on
   fi
 
 fi
