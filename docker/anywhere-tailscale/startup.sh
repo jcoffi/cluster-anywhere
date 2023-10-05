3febda2c94af648c01c43ed66109ab5b9a8692bb
Justin Coffi <jcoffi+github@gmail.com>
2023-10-05 09:00:37 -0700

    added statsmodels pip package back and a funnel for 52365

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -337,8 +337,8 @@ else
 
   #if [ "$LOCATION" != "OnPrem" ]; then
   ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
-  #sudo tailscale serve https:443 / http://localhost:8265 \
-  #&& sudo tailscale funnel 443 on
+  sudo tailscale serve https:443 / http://localhost:52365 \
+  && sudo tailscale funnel 443 on
   #fi
 
 fi
