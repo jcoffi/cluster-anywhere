2d66a94d79b9e79b6e303eafcff6531be69d1732
Justin Coffi <jcoffi+github@gmail.com>
2023-10-23 09:23:44 -0700

    not needed

--- docker/anywhere-tailscale/init.sh
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-/usr/bin/wget -o /home/ray/startup.sh "https://raw.githubusercontent.com/jcoffi/ray/cluster-anywhere/docker/anywhere-tailscale/startup.sh" && /bin/sh -c /home/ray/startup.sh
