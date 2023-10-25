e1ff1a24087c014928f6ed3db214964c609958af
Justin Coffi <jcoffi+github@gmail.com>
2023-10-25 15:15:18 -0700

    forgot a dollar sign

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -345,7 +345,7 @@ else
   #&& sudo tailscale funnel 52365 on
   #fi
 
-  if [ TSOCKS_CONF_FILE ]; then
+  if [ $TSOCKS_CONF_FILE ]; then
     tsocks ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
   fi
 fi
