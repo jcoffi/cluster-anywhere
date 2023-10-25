bc1d87c398bb7bc85b6f8dac4e50e0c752874a75
Justin Coffi <jcoffi+github@gmail.com>
2023-10-25 14:31:44 -0700

    trying tsocks now

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -203,13 +203,19 @@ if [ -c /dev/net/tun ] || [ -c /dev/tun ]; then
 else
     echo "tun doesn't exist"
     sudo tailscaled -port 41641 -statedir $TS_STATEDIR -tun userspace-networking -state mem: -socks5-server=localhost:1055 -outbound-http-proxy-listen=localhost:1055 2>/dev/null&
-    # export socks_proxy=socks5://localhost:1055/
-    # export SOCKS_PROXY=socks5://localhost:1055/
-    # export ALL_PROXY=socks5://localhost:1055/
-    # export http_proxy=http://localhost:1055/
-    # export HTTP_PROXY=http://localhost:1055/
+    export socks_proxy=socks5://localhost:1055/
+    export SOCKS_PROXY=socks5://localhost:1055/
+    export ALL_PROXY=socks5://localhost:1055/
+    export http_proxy=http://localhost:1055/
+    export HTTP_PROXY=http://localhost:1055/
     sudo tailscale up --auth-key=$TS_AUTHKEY --accept-risk=all --accept-routes
-    sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner $USER -j REDIRECT --to-ports 1055
+    cat > /data/tsocks.conf << EOL
+    server = 127.0.0.1
+    server_type = 5
+    server_port = 1055
+    EOL
+
+    export TSOCKS_CONF_FILE=/data/tsocks.conf
 fi
 
 
@@ -337,10 +343,14 @@ elif [ "$NODETYPE" = "user" ]; then
 else
 
   #if [ "$LOCATION" != "OnPrem" ]; then
-  ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net \
+  ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
   #&& sudo tailscale serve tcp:52365 tcp://localhost:52365 \
   #&& sudo tailscale funnel 52365 on
   #fi
+
+  if [ TSOCKS_CONF_FILE ]; then
+    tsocks ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
+  fi
 fi
 
 
