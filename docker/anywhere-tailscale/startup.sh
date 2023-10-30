61eac758d811684c1b6b4a75b88cd7d1aefffd6c
Justin Coffi <jcoffi+github@gmail.com>
2023-10-30 16:29:10 -0700

    another path

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -219,10 +219,10 @@ else
     export HTTP_PROXY=http://localhost:1055/
     sudo tailscale up --auth-key=$TS_AUTHKEY --accept-risk=all --accept-routes
 
-    awk 'NR==1 && /^127.0.0.1/ {print $0 " nexus"} NR!=1 || !/^127.0.0.1/ {print $0}' /etc/hosts > ~/temp_hosts && sudo mv ~/temp_hosts /etc/hosts
+    awk 'NR==1 && /^127.0.0.1/ {print $0 " nexus"} NR!=1 || !/^127.0.0.1/ {print $0}' /etc/hosts > $HOME/temp_hosts && sudo mv $HOME/temp_hosts /etc/hosts
 
 
-    ssh -N -L nexus:6379:nexus:1055 $USER@nexus
+    ssh -N -L nexus:6379:nexus:1055 -D $USER@nexus
 fi
 
 
