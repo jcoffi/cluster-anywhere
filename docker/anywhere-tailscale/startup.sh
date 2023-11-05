07424f5008465d2582787a1bce7bbc3b29f5c603
Justin Coffi <jcoffi+github@gmail.com>
2023-11-05 11:37:50 -0800

    one last time perhaps?

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -132,7 +132,7 @@ check_cloud_provider() {
 if ! grep -q "LOCATION=" /etc/environment; then
   echo "LOCATION=$(check_cloud_provider)" | sudo tee -a /etc/environment
 fi
-export LOCATION=$(check_cloud_provider)
+export LOCATION="$(check_cloud_provider)"
 
 functiontodetermine_cpu() {
   # Check if lscpu command exists
