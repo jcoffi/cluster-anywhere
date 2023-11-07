b783d292a88f07ac8b3a6f658fedc8e0e8316b2b
Justin Coffi <jcoffi+github@gmail.com>
2023-11-07 11:30:03 -0800

    moved RAY_OBJECT_STORE_ALLOW_SLOW_STORAGE=1 to the jupyter notebook

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -322,7 +322,7 @@ if [ "$NODETYPE" = "head" ]; then
   node_master='-Cnode.master=true \\'
   node_data='-Cnode.data=false \\'
 
-  export RAY_OBJECT_STORE_ALLOW_SLOW_STORAGE=1
+
   ray start --head --num-cpus=0 --num-gpus=0 --disable-usage-stats --include-dashboard=True --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net --system-config='{"object_spilling_config":"{\"type\":\"smart_open\",\"params\":{\"uri\":\"gs://cluster-anywhere/ray_job_spill\"}}"}'
 
   sudo tailscale funnel --bg --https 8443 http://localhost:8265
@@ -334,7 +334,7 @@ elif [ ! "$LOCATION" = "OnPrem" ] && [ ! "$NODETYPE" = "head" ]; then
   node_voting_only='-Cnode.voting_only=false \\'
   discovery_zen_minimum_master_nodes='-Cdiscovery.zen.minimum_master_nodes=3'
 
-  export RAY_OBJECT_STORE_ALLOW_SLOW_STORAGE=1
+
   ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
 
 
@@ -348,7 +348,7 @@ elif [ "$NODETYPE" = "user" ]; then
 
   sudo tailscale funnel --bg --https 8443 https+insecure://localhost:8888
 
-  export RAY_OBJECT_STORE_ALLOW_SLOW_STORAGE=1
+
   ray start --address='nexus.chimp-beta.ts.net:6379' --num-gpus=1 --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net --object-store-memory=$ray_object_store
 
   if [ -e "/files" ]; then
@@ -365,10 +365,10 @@ elif [ "$NODETYPE" = "user" ]; then
 else
 
     # if [ ! "$LOCATION" = "OnPrem" ] && [ $ALL_PROXY ]; then
-    #   ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
+    #  ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
     #   #ssh -N -L localhost:6379:localhost:1055 $USER@localhost
     # else
-    export RAY_OBJECT_STORE_ALLOW_SLOW_STORAGE=1
+
     #ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net
     # fi
 
