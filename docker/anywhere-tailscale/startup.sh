e7234894ce9d77f07aaf5c22a28e5e5bf97e203e
Justin Coffi <jcoffi+github@gmail.com>
2023-11-16 13:48:56 -0800

    trying again

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -351,7 +351,7 @@ if [ "$NODETYPE" = "head" ]; then
   ray start --head --num-cpus=0 --num-gpus=0 --disable-usage-stats --include-dashboard=True --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net #--system-config='{"object_spilling_config":"{\"type\":\"smart_open\",\"params\":{\"uri\":\"gs://cluster-anywhere/ray_job_spill\"}}"}'
 
   sudo tailscale funnel --bg --https 443 http://localhost:8265
-  sudo tailscale funnel --bg --tcp 6379 tls-terminated-tcp://localhost:6379
+  sudo tailscale funnel --bg --https 8443 https+insecure://localhost:6379
 
 elif [ "$LOCATION" = "Vast" ]; then
   node_master='-Cnode.master=false \\'
@@ -361,7 +361,7 @@ elif [ "$LOCATION" = "Vast" ]; then
   #There isn't a tun so we can't create a tunnel interface. So we've told cratedb to use eth0.
   sudo sed -i "s/network.publish_host: _tailscale0_/network.publish_host: $IPADDRESS/g" /crate/config/crate.yml
   sudo sed -i "s/_tailscale0_/_eth0_/g" /crate/config/crate.yml
-  ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $IPADDRESS --node-name $HOSTNAME.chimp-beta.ts.net
+  ray start --address='nexus.chimp-beta.ts.net:8443' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $IPADDRESS --node-name $HOSTNAME.chimp-beta.ts.net
 
 
 elif [ ! "$LOCATION" = "OnPrem" ] && [ ! "$NODETYPE" = "head" ]; then
