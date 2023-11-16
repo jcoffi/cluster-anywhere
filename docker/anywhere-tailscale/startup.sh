e451bd72e3f63ef241d79f26ed31a9240acac232
Justin Coffi <jcoffi+github@gmail.com>
2023-11-16 13:31:28 -0800

    another vast attempt

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -359,7 +359,8 @@ elif [ "$LOCATION" = "Vast" ]; then
   node_voting_only='-Cnode.voting_only=false \\'
   discovery_zen_minimum_master_nodes='-Cdiscovery.zen.minimum_master_nodes=3 \\'
   #There isn't a tun so we can't create a tunnel interface. So we've told cratedb to use eth0.
-  sudo sed -i "s/_tailscale0_/$IPADDRESS/g" /crate/config/crate.yml
+  sudo sed -i "s/network.publish_host: _tailscale0_/network.publish_host: $IPADDRESS/g" /crate/config/crate.yml
+  sudo sed -i "s/_tailscale0_/_eth0_/g" /crate/config/crate.yml
   ray start --address='nexus.chimp-beta.ts.net:6379' --resources='{"'"$LOCATION"'": '$(nproc)'}' --disable-usage-stats --dashboard-host 0.0.0.0 --node-ip-address $IPADDRESS --node-name $HOSTNAME.chimp-beta.ts.net
 
 
