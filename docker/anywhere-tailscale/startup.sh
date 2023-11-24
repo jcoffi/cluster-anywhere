41fbb74ae73e036d283e2218266f833c20214aa5
Justin Coffi <jcoffi+github@gmail.com>
2023-11-24 15:17:13 -0800

    pretty sure this still still fail. but we're using IPs to set discovery seed hosts

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -262,6 +262,8 @@ else
     export HTTP_PROXY=http://localhost:3080/
     export https_proxy=http://localhost:3080/
     export HTTPS_PROXY=http://localhost:3080/
+    deviceips=$(curl -s -u "${TSAPIKEY}:" https://api.tailscale.com/api/v2/tailnet/jcoffi.github/devices | jq -r '.addresses[]' data.json | paste -sd, -)
+    discovery_seed_hosts=$deviceips
     sudo tailscale up --auth-key=$TS_AUTHKEY --accept-risk=all --accept-routes --ssh
     sudo sed -i "s/_tailscale0_/_eth0_/g" /crate/config/crate.yml
     export CRATE_JAVA_OPTS="$CRATE_JAVA_OPTS -Dhttps.proxyHost=localhost -Dhttps.proxyPort=3080 -Dhttp.proxyHost=localhost -Dhttp.proxyPort=3080 -DsocksProxyHost=localhost -DsocksProxyPort=1055"
