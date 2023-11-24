5435ba110ffb3aad1bbd3da382ca5ce77e822e46
Justin Coffi <jcoffi+github@gmail.com>
2023-11-24 12:18:52 -0800

    turning off funnel on nexus for 6379

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -354,7 +354,7 @@ if [ "$NODETYPE" = "head" ]; then
   ray start --head --num-cpus=0 --num-gpus=0 --disable-usage-stats --include-dashboard=True --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net #--object-store-memory=$ray_object_store #--system-config='{"object_spilling_config":"{\"type\":\"smart_open\",\"params\":{\"uri\":\"gs://cluster-anywhere/ray_job_spill\"}}"}'
 
   sudo tailscale funnel --bg --https 443 http://localhost:8265
-  sudo tailscale funnel --bg --tcp 8443 tcp://localhost:6379
+  #sudo tailscale funnel --bg --tcp 8443 tcp://localhost:6379
 
 
 elif [ "$LOCATION" = "Vast" ]; then
