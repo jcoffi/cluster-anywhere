b9f83468f2483fd0cd8abbcd031c8f3ac9f8d715
Justin Coffi <jcoffi+github@gmail.com>
2023-11-24 16:51:17 -0800

    turned off crate run test for Vast location

--- docker/anywhere-tailscale/run_tests.sh
+++ docker/anywhere-tailscale/run_tests.sh
@@ -42,9 +42,10 @@ if [ "$LOCATION" != "OnPrem" ]; then
 fi
 
 # Check Crate.io status
-crate_status=$(curl -s -I http://localhost:4200/ | grep HTTP/1.1)
-if [[ "$crate_status" != *"200 OK"* ]]; then FAIL=1; fi
-
+if [ "$LOCATION" != "Vast" ]; then
+    crate_status=$(curl -s -I http://localhost:4200/ | grep HTTP/1.1)
+    if [[ "$crate_status" != *"200 OK"* ]]; then FAIL=1; fi
+fi
 
 # Write status to a file
 # also write a bit of additional informationffor diagnostic purposes.
