ca3b4a49f5c2cb36556883bd9ad197a000c07099
Justin Coffi <jcoffi+github@gmail.com>
2023-11-24 16:47:59 -0800

    forgot a couple ports for proxy stuff

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -268,7 +268,7 @@ else
 
     sudo tailscale up --operator='ray' --auth-key=$TS_AUTHKEY --accept-risk=all --accept-routes --ssh
     sudo sed -i "s/_tailscale0_/_eth0_/g" /crate/config/crate.yml
-    export CRATE_JAVA_OPTS="$CRATE_JAVA_OPTS -Dhttps.proxyHost=localhost -Dhttps.proxyPort=3080 -Dhttp.proxyHost=localhost -Dhttp.proxyPort=3080 -DsocksProxyHost=localhost -DsocksProxyPort=1055"
+    export CRATE_JAVA_OPTS="$CRATE_JAVA_OPTS -Dhttps.proxyHost=localhost -Dhttps.proxyPort=1055 -Dhttp.proxyHost=localhost -Dhttp.proxyPort=1055 -DsocksProxyHost=localhost -DsocksProxyPort=1055"
     echo "nameserver 100.100.100.100" | sudo tee /etc/resolv.conf
     echo "search chimp-beta.ts.net" | sudo tee -a /etc/resolv.conf
     export RAY_grpc_enable_http_proxy="1"
