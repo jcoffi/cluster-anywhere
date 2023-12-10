a05fa975c5696049a744e942703bab5752c97b3d
Justin Coffi <jcoffi+github@gmail.com>
2023-12-10 12:09:34 -0800

    turned off spill to bucket

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -361,7 +361,7 @@ if [ "$NODETYPE" = "head" ]; then
   node_data='-Cnode.data=false \\'
 
 
-  ray start --head --num-cpus=0 --num-gpus=0 --disable-usage-stats --include-dashboard=True --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net --system-config='{"object_spilling_config":"{\"type\":\"smart_open\",\"params\":{\"uri\":\"gs://cluster-anywhere/ray_job_spill\"}}"}'
+  ray start --head --num-cpus=0 --num-gpus=0 --disable-usage-stats --include-dashboard=True --dashboard-host 0.0.0.0 --node-ip-address $HOSTNAME.chimp-beta.ts.net --node-name $HOSTNAME.chimp-beta.ts.net #--system-config='{"object_spilling_config":"{\"type\":\"smart_open\",\"params\":{\"uri\":\"gs://cluster-anywhere/ray_job_spill\"}}"}'
 
   sudo tailscale funnel --bg --https 443 http://localhost:8265
   #sudo tailscale funnel --bg --tcp 8443 tcp://localhost:6379
