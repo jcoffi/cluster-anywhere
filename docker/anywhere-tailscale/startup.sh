a1bb81b9f53c950e202e7fd40b4b42aabbe418fb
Justin Coffi <jcoffi+github@gmail.com>
2025-02-07 12:45:43 -0800

    turned off update base again

--- docker/anywhere-tailscale/startup.sh
+++ docker/anywhere-tailscale/startup.sh
@@ -419,7 +419,7 @@ elif [ "$NODETYPE" = "user" ]; then
   conda config --set pip_interop_enabled false
   conda install -n base --solver=classic -y conda-libmamba-solver
   conda config --set solver libmamba
-  conda update -n base -c defaults
+  #conda update -n base -c defaults
   conda config --append channels rapidsai
   conda config --append channels conda-forge
   export JUPYTERLAB_SETTINGS_DIR='/data/.jupyter/lab/user-settings/'
